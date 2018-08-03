4ded03975492ded5b4b339eb24f8a50bcf540bd8
Prajit.Gandhi <prajit.gandhi@cognizant.com>
2018-08-03 20:40:35 +0530

    Update EnvironmentVariables.sh

--- movie-cruiser-app/EnvironmentVariables.sh
+++ movie-cruiser-app/EnvironmentVariables.sh
@@ -1,4 +1,4 @@
-export MYSQL_MOVIE_DB_USERNAME = root 
-export MYSQL_MOVIE_DB_PASSWORD = root 
-export MYSQL_MOVIE_DB_URL = jdbc:mysql://localhost:3306/MovieCruiser
-export MYSQL_MOVIE_DB_NAME = MovieCruiser
+export MYSQL_MOVIE_DB_USERNAME root 
+export MYSQL_MOVIE_DB_PASSWORD root 
+export MYSQL_MOVIE_DB_URL jdbc:mysql://localhost:3306/MovieCruiser
+export MYSQL_MOVIE_DB_NAME MovieCruiser
