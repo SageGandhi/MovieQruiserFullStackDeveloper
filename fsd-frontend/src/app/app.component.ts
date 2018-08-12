0143d956240e395eb061cb034657d6f32a0db2ec
SageGandhi <prajit.ju@gmail.com>
2018-08-13 02:14:04 +0530

    Ok Component

--- fsd-frontend/src/app/app.component.ts
+++ fsd-frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HelloAngularComponent} from './hello-angular/hello-angular.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
