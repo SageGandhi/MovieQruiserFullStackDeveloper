4b0348543faf8f8228aaad468562bc576309b3ae
SageGandhi <prajit.ju@gmail.com>
2018-08-23 05:44:43 +0530

    LogOut Test

---
+++ fsd-frontend/src/app/modules/authentication/components/logout/logout.component.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '../../service/authentication.service';
+import { MaterialModule } from '../../../material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { By } from "@angular/platform-browser";
+import { LogoutComponent } from './logout.component';
+
+class LoginComponentFixture { }
+
+describe('LogoutComponent Unit Test', () => {
+  let component: LogoutComponent;
+  let fixture: ComponentFixture<LogoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LogoutComponent],
+      providers: [AuthenticationService],
+      imports: [MaterialModule, FormsModule, HttpClientModule,
+        RouterTestingModule.withRoutes([
+          {path: '', redirectTo:'login',pathMatch:'full'},
+          {path: 'login', component: LoginComponentFixture}
+        ])]
+    }).compileComponents();
+  }));
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(LogoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('LogoutComponent Is Truthy', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Service Injected Via Component Should Be Instance Of AuthenticationService', () => {
+    expect(TestBed.get(AuthenticationService) instanceof AuthenticationService).toBeTruthy();
+  });
+});
